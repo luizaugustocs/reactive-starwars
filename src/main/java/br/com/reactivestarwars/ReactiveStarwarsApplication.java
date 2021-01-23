d2e2dc53a412bbf9fd6d909a62c6529c0ead7edf
Luiz Augusto C. Souza <luizaugustocsouza@gmail.com>
2021-01-23 10:19:20 -0300

    Initial commit

---
+++ src/main/java/br/com/reactivestarwars/ReactiveStarwarsApplication.java
@@ -0,0 +1,13 @@
+package br.com.reactivestarwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveStarwarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveStarwarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/reactivestarwars/ReactiveStarwarsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.reactivestarwars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveStarwarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
